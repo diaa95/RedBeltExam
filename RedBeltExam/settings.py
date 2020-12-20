3900c0622f2305ad252b830de7393c9543d70d6c
Diaa AbdAlDayem <Diaa.abdaldayem@gmail.com>
2020-12-20 08:17:33 +0200

    second commit

--- RedBeltExam/settings.py
+++ RedBeltExam/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8vr5zhhsuz5fdv17esgg+_zah+klhke)vt85x*4)&*22zkd$bz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.140.194.231']
 
 
 # Application definition
